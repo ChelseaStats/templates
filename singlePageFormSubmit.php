fba4ac2f057d27719100015bb4222699a7a309c0
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2016-03-31 09:24:33 +0100

    Update singlePageFormSubmit.php

--- singlePageFormSubmit.php
+++ singlePageFormSubmit.php
@@ -1,22 +1,21 @@
 <?php /* Template Name:  Single Page Form Submit */ ?>
-			<?php
-				$hidden     = $_POST['hidden'];
-		
-				if(isset($hidden) && $hidden == 'hidden') {
-				
-				    // process form stuff here
-						
-				} else {
-			?>
-					<form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>">
-							<div class="form-group">
-								<label for="formitem1">formitem1:</label>
-								<input name="formitem1"   type="text" id="formitem1" placeholder="formitem1" value="formitem1" />
-							</div>
-							<div class="form-group">
-								<input type="hidden" value="hidden" name="hidden">
-								<input type="submit" value="submit" class="btn btn-primary">
-							</div>
-					</form>
-			<?php } ?>
-		</div>
+<?php
+	$hidden     = $_POST['hidden'];
+
+	if(isset($hidden) && $hidden == 'hidden') {
+	
+	    // process form stuff here
+			
+	} else {
+?>
+		<form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>">
+				<div class="form-group">
+					<label for="formitem1">formitem1:</label>
+					<input name="formitem1"   type="text" id="formitem1" placeholder="formitem1" value="formitem1" />
+				</div>
+				<div class="form-group">
+					<input type="hidden" value="hidden" name="hidden">
+					<input type="submit" value="submit" class="btn btn-primary">
+				</div>
+		</form>
+<?php } ?>
